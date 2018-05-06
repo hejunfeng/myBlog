b0fe857ea12b1c53d983dd7c24ed983e02c076b1
kaka19881111 <hejunfeng1090@163.com>
2018-05-06 12:32:49 +0800

    fix eslit error

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var createError = require('http-errors');
+const createError = require('http-errors');
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -24,16 +25,20 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function(err, req, res) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+app.listen(3000,()=>{
+	console.log('server start');
 });

f01053bf462d0ae623e462d4b9224d5031c9ac8f
kaka19881111 <hejunfeng1090@163.com>
2018-05-28 22:17:12 +0800

    basic design

--- README.md
+++ README.md
@@ -2,31 +2,29 @@
 this is a blog for demo and learn security topics.
 ## 功能与路由设计
 
-在开发博客之前，我们首先需要明确博客要实现哪些功能。
-
-功能及路由设计如下：
-
 1. 注册
-    1. 注册页：`GET /register`
-    2. 注册（包含上传头像）：`POST /register`
+    1. 注册页：`GET /signup`
+    2. 注册（包含上传头像）：`POST /signup`
 2. 登录
-    1. 登录页：`GET /login`
-    2. 登录：`POST /login`
-3. 登出：`GET /logout`
+    1. 登录页：`GET /signin`
+    2. 登录：`POST /signin`
+3. 登出：`GET /signout`
 4. 查看文章
     1. 主页：`GET /posts`
-    2. 个人主页：`GET /posts/:author`
+    2. 个人主页：`GET /posts?author=xxx`
     3. 查看一篇文章（包含留言）：`GET /posts/:postId`
 5. 发表文章
-    1. 发表文章页：`GET /posts/`
-    2. 发表文章：`POST /posts`
+    1. 发表文章页：`GET /posts/create`
+    2. 发表文章：`POST /posts/create`
 6. 修改文章
-    1. 修改文章页：`GET /posts/:postId`
-    2. 修改文章：`PUT /posts/:postId`
-7. 删除文章：`DELETE /posts/:postId`
+    1. 修改文章页：`GET /posts/:postId/edit`
+    2. 修改文章：`POST /posts/:postId/edit`
+7. 删除文章：`GET /posts/:postId/remove`
 8. 留言
     1. 创建留言：`POST /comments`
-    2. 删除留言：`DELETE /comments/:commentId`
+    2. 删除留言：`GET /comments/:commentId/remove`
+
+由于我们博客页面是后端渲染的，所以只通过简单的 `<a>(GET)` 和 `<form>(POST)` 与后端进行交互，如果使用 jQuery 或者其他前端框架（如 Angular、Vue、React 等等）可通过 Ajax 与后端交互，则 api 的设计应尽量遵循 Restful 风格。
 
 #### Restful
 
--- app.js
+++ app.js
@@ -1,44 +1,41 @@
-const createError = require('http-errors');
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+const config = require('config-lite')(__dirname);
+const routes = require('./routes');
+const pkg = require('./package');
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const app = express();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
+// 设置模板目录
 app.set('views', path.join(__dirname, 'views'));
+// 设置模板引擎为 ejs
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
+// session 中间件
+app.use(session({
+	name: config.session.key, // 设置 cookie 中保存 session id 的字段名称
+	secret: config.session.secret, // 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+	resave: true, // 强制更新 session
+	saveUninitialized: false, // 设置为 false，强制创建一个 session，即使用户未登录
+	cookie: {
+		maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
+	},
+	store: new MongoStore({// 将 session 存储到 mongodb
+		url: config.mongodb// mongodb 地址
+	})
+}));
+// flash 中间件，用来显示通知
+app.use(flash());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// 路由
+routes(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-	next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
-});
-
-app.listen(3000,()=>{
-	console.log('server start');
+// 监听端口，启动程序
+app.listen(config.port, function () {
+	console.log(`${pkg.name} listening on port ${config.port}`);
 });
